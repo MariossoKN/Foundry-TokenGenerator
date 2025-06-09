89007f176976d6220d62f95d075bde6d1863c45f
Mariosso <marian.hencz@gmail.com>
2025-06-09 19:35:52 +0200

    Updated mock.

--- script/MockWETH.s.sol
+++ script/MockWETH.s.sol
@@ -1,22 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
 
-// Mock WETH contract
-contract MockWETH is ERC20 {
-    constructor() ERC20("Wrapped Ether", "WETH") {}
+import {SafeTransferLib} from "lib/solmate/src/utils/SafeTransferLib.sol";
 
-    function deposit() external payable {
+/// @notice Minimalist and modern Wrapped Ether implementation.
+/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/tokens/WETH.sol)
+/// @author Inspired by WETH9 (https://github.com/dapphub/ds-weth/blob/master/src/weth9.sol)
+contract WETH is ERC20("Wrapped Ether", "WETH", 18) {
+    using SafeTransferLib for address;
+
+    event Deposit(address indexed from, uint256 amount);
+
+    event Withdrawal(address indexed to, uint256 amount);
+
+    function deposit() public payable virtual {
         _mint(msg.sender, msg.value);
+
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 amount) external {
+    function withdraw(uint256 amount) public virtual {
         _burn(msg.sender, amount);
-        payable(msg.sender).transfer(amount);
+
+        emit Withdrawal(msg.sender, amount);
+
+        msg.sender.safeTransferETH(amount);
     }
 
-    // receive() external payable {
-    //     deposit();
-    // }
+    receive() external payable virtual {
+        deposit();
+    }
 }
